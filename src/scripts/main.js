f05c35aa51e449a35380982cfc4799d7ef191d63
Hans Fast <hans@surfacemarkup.net>
2016-09-24 19:29:32 +0200

    minimal setup that results in bug

---
+++ src/scripts/main.js
@@ -0,0 +1,23 @@
+//this is a comment, y'all
+//this is another comment
+//this is a third comment
+let foo = 'a message'
+
+
+//harmless comment
+//another harmless comment
+
+function saySomething(message) {
+  console.log('I like to use ' + message);
+  hello();
+}
+
+function hello() {
+  return false
+}
+
+saySomething(foo);
+saySomething(foo);
+
+
+
